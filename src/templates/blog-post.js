dfd20e28d279744c6f511fa4a8d1e987ba68f764
Michael Bonner <michael@redolive.com>
2019-07-05 22:48:19 -0600

    add image to blog post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { kebabCase } from 'lodash'
-import Helmet from 'react-helmet'
-import { graphql, Link } from 'gatsby'
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
-import RequestAQuote from '../components/RequestAQuote';
+import React from "react";
+import PropTypes from "prop-types";
+import { kebabCase } from "lodash";
+import Helmet from "react-helmet";
+import { graphql, Link } from "gatsby";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
+import RequestAQuote from "../components/RequestAQuote";
+import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 
 export const BlogPostTemplate = ({
   content,
@@ -13,19 +14,26 @@ export const BlogPostTemplate = ({
   description,
   tags,
   title,
-  helmet,
+  featuredimage,
+  helmet
 }) => {
-  const PostContent = contentComponent || Content
+  const PostContent = contentComponent || Content;
 
   return (
-    <section className="pt-12">
-      {helmet || ''}
-      <div className="container m-auto mb-12 content">
+    <section>
+      {helmet || ""}
+      <div className="xl:px-16 m-auto">
+        <PreviewCompatibleImage
+          imageInfo={{
+            image: featuredimage,
+            alt: `featured image thumbnail for post ${title}`
+          }}
+        />
+      </div>
+      <div className="container m-auto mb-12 content px-4 md:px-0 pt-4">
         <div className="columns">
           <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 font-bold is-bold-light">
-              {title}
-            </h1>
+            <h1 className="title is-size-2 font-bold is-bold-light">{title}</h1>
             <p>{description}</p>
             <PostContent content={content} />
             {tags && tags.length ? (
@@ -45,19 +53,19 @@ export const BlogPostTemplate = ({
       </div>
       <RequestAQuote />
     </section>
-  )
-}
+  );
+};
 
 BlogPostTemplate.propTypes = {
   content: PropTypes.node.isRequired,
   contentComponent: PropTypes.func,
   description: PropTypes.string,
   title: PropTypes.string,
-  helmet: PropTypes.object,
-}
+  helmet: PropTypes.object
+};
 
 const BlogPost = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
 
   return (
     <Layout>
@@ -76,18 +84,19 @@ const BlogPost = ({ data }) => {
         }
         tags={post.frontmatter.tags}
         title={post.frontmatter.title}
+        featuredimage={post.frontmatter.featuredimage}
       />
     </Layout>
-  )
-}
+  );
+};
 
 BlogPost.propTypes = {
   data: PropTypes.shape({
-    markdownRemark: PropTypes.object,
-  }),
-}
+    markdownRemark: PropTypes.object
+  })
+};
 
-export default BlogPost
+export default BlogPost;
 
 export const pageQuery = graphql`
   query BlogPostByID($id: String!) {
@@ -97,9 +106,16 @@ export const pageQuery = graphql`
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         title
+        featuredimage {
+          childImageSharp {
+            fluid(maxWidth: 120, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
         description
         tags
       }
     }
   }
-`
+`;
